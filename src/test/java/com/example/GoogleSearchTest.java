1d85d05db46e29b8de1617627f93ccbb77d565d0
Israfil Alam <israfil@ba-systems.com>
2024-06-02 14:08:43 +0600

    updated

--- src/test/java/com/example/GoogleSearchTest.java
+++ src/test/java/com/example/GoogleSearchTest.java
@@ -1,17 +1,23 @@
-package com.example;
-
-import org.junit.Assert;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GoogleSearchTest {
-    @Test
-    public void testGoogleSearch() {
+    public static void main(String[] args) {
+        // Set the path to the ChromeDriver executable
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        Assert.assertEquals("Google", driver.getTitle());
+
+        // Setup Chrome options
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless"); // Run in headless mode if necessary
+
+        // Initialize ChromeDriver
+        WebDriver driver = new ChromeDriver(options);
+        
+        // Your test code here
+
         driver.quit();
     }
 }
