af50c3cabacf678e0fe4ab340e7a4caac88fa65f
Israfil Alam <israfil@ba-systems.com>
2024-06-02 13:44:03 +0600

    updated

--- src/test/java/com/example/GoogleSearchTest.java
+++ src/test/java/com/example/GoogleSearchTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class GoogleSearchTest {
     @Test
     public void testGoogleSearch() {
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
         Assert.assertEquals("Google", driver.getTitle());
